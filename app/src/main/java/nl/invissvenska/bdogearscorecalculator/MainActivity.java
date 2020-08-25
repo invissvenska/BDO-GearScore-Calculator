bc3e4442ad4cfd7e95efcc354d1c93959ddda63a
Sven van den Tweel <sven.vd.tweel@gmail.com>
2020-08-25 17:28:51 +0200

    replaced app icon

--- app/src/main/java/nl/invissvenska/bdogearscorecalculator/MainActivity.java
+++ app/src/main/java/nl/invissvenska/bdogearscorecalculator/MainActivity.java
@@ -5,54 +5,54 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView navigationView;
-
     private CalculatorFragment calculatorFragment;
     private BracketFragment bracketFragment;
+
+    private static final String CALCULATOR = "Calculator";
+    private static final String BRACKET = "Brackets";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        calculatorFragment = (CalculatorFragment) getSupportFragmentManager().findFragmentByTag("Calculator");
-        bracketFragment = (BracketFragment) getSupportFragmentManager().findFragmentByTag("Brackets");
+        calculatorFragment = (CalculatorFragment) getSupportFragmentManager().findFragmentByTag(CALCULATOR);
+        bracketFragment = (BracketFragment) getSupportFragmentManager().findFragmentByTag(BRACKET);
 
         if (calculatorFragment == null || bracketFragment == null) {
             calculatorFragment = new CalculatorFragment();
             bracketFragment = new BracketFragment();
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.nav_host_fragment, calculatorFragment, "Calculator")
-                    .commit();
+            setFragment(calculatorFragment, CALCULATOR);
         }
 
-        navigationView = findViewById(R.id.bottom_nav);
+        BottomNavigationView navigationView = findViewById(R.id.bottom_nav);
         navigationView.setSelectedItemId(R.id.action_calculator);
         navigationView.setOnNavigationItemSelectedListener((@NonNull MenuItem item) -> {
             int id = item.getItemId();
 
             if (id == R.id.action_calculator) {
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.nav_host_fragment, calculatorFragment, "Calculator")
-                        .commit();
+                setFragment(calculatorFragment, CALCULATOR);
                 return true;
             }
             if (id == R.id.action_brackets) {
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.nav_host_fragment, bracketFragment, "Brackets")
-                        .commit();
+                setFragment(bracketFragment, BRACKET);
                 return true;
             }
             return false;
         });
+    }
+
+    private void setFragment(Fragment fragment, String tag) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.nav_host_fragment, fragment, tag)
+                .commit();
     }
 
     @Override
