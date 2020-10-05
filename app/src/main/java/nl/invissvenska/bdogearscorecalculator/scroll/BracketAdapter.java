594ec1eb9d95502faf125a6788293b5d776925f8
Sven van den Tweel <sven.vd.tweel@gmail.com>
2020-10-05 20:07:25 +0200

    fixed tests

--- app/src/main/java/nl/invissvenska/bdogearscorecalculator/scroll/BracketAdapter.java
+++ app/src/main/java/nl/invissvenska/bdogearscorecalculator/scroll/BracketAdapter.java
@@ -17,13 +17,11 @@ public class BracketAdapter extends RecyclerView.Adapter<BracketAdapter.BracketI
     private List<BracketItem> brackets = new ArrayList<>();
 
     private String title;
-    private Calculator calculator;
 
     public BracketAdapter(Calculator calculator, final String title) {
-        this.calculator = calculator;
         this.title = title;
         this.brackets.clear();
-        this.brackets.addAll(this.calculator.getBrackets());
+        this.brackets.addAll(calculator.getBrackets());
         notifyDataSetChanged();
     }
 
@@ -37,25 +35,10 @@ public class BracketAdapter extends RecyclerView.Adapter<BracketAdapter.BracketI
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         ItemBracketBinding itemBinding = ItemBracketBinding.inflate(layoutInflater, parent, false);
         return new BracketItemViewHolder(itemBinding);
-
     }
 
     @Override
     public void onBindViewHolder(@NonNull BracketItemViewHolder holder, int position) {
-//        final BracketItem item = brackets.get(position);
-//        Context context = holder.fromTo.getContext();
-//        if (item.getTo().equals(999)) {
-//            holder.fromTo.setText(context.getString(R.string.bracket_value_max, item.getFrom()));
-//        } else {
-//            holder.fromTo.setText(context.getString(R.string.bracket_value, item.getFrom(), item.getTo()));
-//        }
-//        holder.bonus.setText(context.getString(R.string.bracket_bonus, item.getPrefix(), item.getBonus(), item.getSuffix()));
-//        holder.bonusSum.setText(calculator.calculate(item.getFrom()).toString());
-
-//        ItemBracketBinding binding = holder.getBinding();
-//        binding.setBracket((BracketItem)this.brackets.get(position));
-//        binding.executePendingBindings();
-
         BracketItem item = this.brackets.get(position);
         holder.bind(item);
     }
@@ -65,8 +48,7 @@ public class BracketAdapter extends RecyclerView.Adapter<BracketAdapter.BracketI
         return brackets.size();
     }
 
-
-    class BracketItemViewHolder extends RecyclerView.ViewHolder {
+    static class BracketItemViewHolder extends RecyclerView.ViewHolder {
 
         public final ItemBracketBinding binding;
 
