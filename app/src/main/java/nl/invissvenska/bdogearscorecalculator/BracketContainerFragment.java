ad2f0f2fb384e8ecf6b8d935ab63b30a72b6cf56
Sven van den Tweel <sven.vd.tweel@gmail.com>
2021-02-19 20:47:24 +0100

    wip

--- app/src/main/java/nl/invissvenska/bdogearscorecalculator/BracketContainerFragment.java
+++ app/src/main/java/nl/invissvenska/bdogearscorecalculator/BracketContainerFragment.java
@@ -11,6 +11,10 @@ import androidx.fragment.app.Fragment;
 
 public class BracketContainerFragment extends Fragment {
 
+    private static final String BRACKET_AP = "APBrackets";
+
+    private BracketAPFragment bracketAPFragment;
+
     public BracketContainerFragment() {
         // keep default public constructor
     }
@@ -18,6 +22,33 @@ public class BracketContainerFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.brackets_container, container, false);
+        View view = inflater.inflate(R.layout.brackets_container, container, false);
+
+//        bracketAPFragment = (BracketAPFragment) getActivity().getSupportFragmentManager().findFragmentByTag(BRACKET_AP);
+//
+//        if (bracketAPFragment == null) {
+//            bracketAPFragment = new BracketAPFragment();
+//        }
+//        setFragment(bracketAPFragment, R.id.bracketsAP, BRACKET_AP);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        bracketAPFragment = (BracketAPFragment) getActivity().getSupportFragmentManager().findFragmentByTag(BRACKET_AP);
+
+        if (bracketAPFragment == null) {
+            bracketAPFragment = new BracketAPFragment();
+        }
+        setFragment(bracketAPFragment, R.id.bracketsAP, BRACKET_AP);
+    }
+
+    private void setFragment(Fragment fragment, int container, String tag) {
+        getActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .replace(container, fragment, tag)
+                .commit();
     }
 }
